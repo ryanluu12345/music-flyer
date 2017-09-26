a6e0b1163f57549080cc281fe2cf97ad1a19441b
RyanAveo <ryan.aveo@gmail.com>
2017-09-26 01:12:58 -0700

    Added home page

---
+++ static/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $(function() {
+        $('form').each(function() {
+            $(this).find('input').keypress(function(e) {
+                // Enter pressed?
+                if(e.which == 10 || e.which == 13) {
+                    this.form.submit();
+                }
+            });
+
+            $(this).find('input[type=submit]').hide();
+        });
+    });
+});
