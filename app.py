55e3d113467836a9383f04e5e4ccea9ad1053785
ryanluu12345 <ryanluu12345@gmail.com>
2017-09-27 23:33:44 -0700

    Add files via upload
    Integrated data from API into web page

--- app.py
+++ app.py
@@ -1,54 +1,65 @@
 from flask import Flask,render_template,url_for,request,redirect
 from genius import genius
+from spotify import search
 
 app=Flask(__name__)
 
 #Creates global lists that house artist information
 blurbs=[]
-artistFbs=[]
-artistTwitters=[]
-artistInstas=[]
-artistImgs=[]
-artistDescriptions=[]
+artistFbs=""
+artistTwitters=""
+artistInstas=""
+artistImg=""
+artistDescriptions=""
+artistSongs=""
 
 #Variable for chosen artist info
 artistName="Your Artist"
 
 @app.route("/",methods=["POST","GET"])
 def index():
-    global artistName
+    global artistName,artistImg,artistSongs
 
     if request.method=="POST":
-        artistName=request.form["artistName"]
+        artistName=cleanString(request.form["artistName"])
+
+        # Initializes genius and spotify to get data from the APIs
+        GeniusCreator = genius()
+        SpotifyCreator= search()
+
+        SpotifyCreator.getAccessToken()
+        SpotifyCreator.searchArtist(artistName)
+        SpotifyCreator.searchArtistTopTracks()
+        artistImg=SpotifyCreator.getArtistImage()
+        artistSongs=SpotifyCreator.getSongNames()
+
+        # Put this all in a loop later with all of the available songs
+        songs = ["Yellow Coldplay", "Thats what I like", "Broke Lecrae", "Love me now John Legend"]
+        blurb, artistFb, artistTwitter, artistInsta, artistImgNone, artistDescription = GeniusCreator.getArtistInfo(
+            artistName)
+        addToLists(blurb)
+
         return redirect(url_for("artists"))
 
-    #Initializes genius to get data from the API
-    GeniusCreator=genius()
 
-    #Put this all in a loop later with all of the available songs
-    songs=["Yellow Coldplay","Thats what I like","Broke Lecrae","Love me now John Legend"]
-    blurb, artistFb, artistTwitter, artistInsta, artistImg,artistDescription =GeniusCreator.getArtistInfo(songs[0])
-    addToLists(blurb,artistFb,artistTwitter,artistInsta,artistImg,artistDescription)
-
-    return render_template("index.html",blurb=blurb,artistFb=artistFb,artistTwitter=artistTwitter,artistInsta=artistInsta)
+    return render_template("index.html")
 
 
 @app.route("/artists")
 def artists():
-    global artistName
-
-    return render_template("flyer.html",yourArtist=artistName)
+    global artistName,artistImg,artistSongs
+    return render_template("flyer.html",yourArtist=artistName,artistImg=artistImg,artistSongs=artistSongs)
 
 
 #Method that appends items to a list since the list will later be used to provide info on a webpage
-def addToLists(blurb,artistFb,artistTwitter,artistInsta,artistImg,artistDescription):
+def addToLists(blurb):
     blurbs.append(blurb)
-    artistFbs.append(artistFb)
-    artistTwitters.append(artistTwitter)
-    artistInstas.append(artistInsta)
-    artistImgs.append(artistImg)
-    artistDescriptions.append(artistDescription)
 
+
+def cleanString(string):
+
+    string=string.split(" ")
+    return ' '.join([word.capitalize() for word in string])
 
 
 
