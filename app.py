a675fa357db76070cff2435927507a9d7fb39878
ryanluu12345 <ryanluu12345@gmail.com>
2017-10-14 02:04:06 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask,render_template,url_for,request,redirect
 from genius import genius
-from spotify import search
+from musicFlyer2 import search
 
 app=Flask(__name__)
 
@@ -10,15 +10,19 @@ artistFbs=""
 artistTwitters=""
 artistInstas=""
 artistImg=""
-artistDescriptions=""
+artistDescription=""
 artistSongs=""
+artistTopSongs=""
+errorText=None
 
 #Variable for chosen artist info
 artistName="Your Artist"
 
+#Routes to the index
 @app.route("/",methods=["POST","GET"])
 def index():
-    global artistName,artistImg,artistSongs
+    #Accesses global variables defined outside the function
+    global artistName,artistImg,artistSongs,artistTopSongs,artistDescription,errorText
 
     if request.method=="POST":
         artistName=cleanString(request.form["artistName"])
@@ -27,35 +31,51 @@ def index():
         GeniusCreator = genius()
         SpotifyCreator= search()
 
-        SpotifyCreator.getAccessToken()
-        SpotifyCreator.searchArtist(artistName)
-        SpotifyCreator.searchArtistTopTracks()
-        artistImg=SpotifyCreator.getArtistImage()
-        artistSongs=SpotifyCreator.getSongNames()
+        #Accesses all information about a given artist
+        try:
+            SpotifyCreator.getAccessToken()
+            SpotifyCreator.searchArtist(artistName)
+            SpotifyCreator.searchArtistEmbed()
+            SpotifyCreator.searchArtistTopTracks()
+            SpotifyCreator.searchRelatedArtists()
+            artistImg=SpotifyCreator.getArtistImage()
+            artistSongs=SpotifyCreator.getSongNames()
+            artistTopSongs=SpotifyCreator.getArtistEmbed()
+            artistRelated=SpotifyCreator.getRelatedArtists()
 
-        # Put this all in a loop later with all of the available songs
-        songs = ["Yellow Coldplay", "Thats what I like", "Broke Lecrae", "Love me now John Legend"]
-        blurb, artistFb, artistTwitter, artistInsta, artistImgNone, artistDescription = GeniusCreator.getArtistInfo(
-            artistName)
-        addToLists(blurb)
+            print(artistRelated)
 
-        return redirect(url_for("artists"))
+            blurb, artistFb, artistTwitter, artistInsta, artistImgNone, artistDescription = GeniusCreator.getArtistInfo(
+                artistName)
+            addToLists(artistInsta)
+            addToLists(artistFb)
+            addToLists(blurb)
+            addToLists(artistRelated)
+
+            #Redirects to the artist page with all the info of artists
+            return redirect(url_for("artists"))
+
+        except:
+            errorText="Please enter a valid artist name!"
+            return render_template("index.html", errorText=errorText)
+
+    return render_template("index.html",errorText=errorText)
 
 
-    return render_template("index.html")
-
-
+#Routes to artist page with all the info and links to pages
 @app.route("/artists")
 def artists():
-    global artistName,artistImg,artistSongs
-    return render_template("flyer.html",yourArtist=artistName,artistImg=artistImg,artistSongs=artistSongs)
+    global artistName,artistImg,artistSongs,artistTopSongs,artistDescription,errorText,blurbs
+    return render_template("flyer.html",yourArtist=artistName,artistImg=artistImg,artistSongs=artistSongs,artistTopSongs=artistTopSongs,artistInsta=blurbs[0],artistFb=blurbs[1],blurb=blurbs[2],relatedArtists=blurbs[3])
 
 
 #Method that appends items to a list since the list will later be used to provide info on a webpage
 def addToLists(blurb):
+    global blurbs
     blurbs.append(blurb)
 
 
+#Cleans strings in order to get a capitalized version of words
 def cleanString(string):
 
     string=string.split(" ")
